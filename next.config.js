031586468e066d9311e22f2ae11c728fbc11a9d6
Natan Weinberger <natan.weinberger@shopify.com>
2022-03-06 21:56:15 -0500

    Upgrade tailwind, add basic MDX

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
-const {withContentlayer} = require('next-contentlayer')
-
-module.exports = withContentlayer()({})
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
+module.exports = withMDX({
+  // Append the default value with md extensions
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,16 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,jsx,mdx}',
+    './components/**/*.{js,jsx,mdx}',
+  ],
+  darkMode: 'media', // or 'false' or 'class'
   theme: {
-    extend: {
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            color: theme('colors.gray.700'),
-            a: {
-              color: theme('colors.blue.500'),
-              '&:hover': {
-                color: theme('colors.blue.700')
-              },
-              code: { color: theme('colors.blue.400') }
-            },
-            thead: {
-              borderBottomColor: theme('colors.gray.200')
-            },
-            code: { color: theme('colors.pink.500') },
-            'blockquote p:first-of-type::before': false,
-            'blockquote p:last-of-type::after': false
-          }
-        },
-      })
-    },
+    extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [
-   require('@tailwindcss/typography')
+    require('@tailwindcss/typography')
   ],
 };
