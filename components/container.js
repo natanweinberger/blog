087b1f34d7d36f5d29a2966475a3e54b444e067a
Natan Weinberger <natan.weinberger@shopify.com>
2022-03-23 16:52:53 -0400

    Make container body fill remainder of screen

--- components/container.js
+++ components/container.js
@@ -2,14 +2,14 @@ import Head from 'next/head';
 import Header from '@/components/header';
 
 const Container = ({ children }) => (
-  <div className="flex flex-col items-center h-screen text-black dark:text-white bg-white dark:bg-black px-8">
+  <div className="flex flex-col items-center h-screen text-gray-700 dark:text-gray-300 bg-white dark:bg-black px-8">
     <Head>
       <title>Natan Weinberger</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <div className="w-full max-w-2xl text-gray-700">
-    <Header />
-    <main>{children}</main>
+    <div className="w-full grow flex flex-col max-w-2xl">
+      <Header />
+      <main className="grow">{children}</main>
     </div>
   </div>
 );
