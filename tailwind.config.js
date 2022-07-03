34df315267e47bc11dc3adb4334a1aaec63c6259
Natan Weinberger <natan.weinberger@shopify.com>
2022-07-03 11:24:24 -0400

    Add custom code styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,30 @@ module.exports = {
   ],
   darkMode: 'media', // or 'false' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+          code: {
+            backgroundColor: theme("colors.gray.100"),
+            color: "#DD1144",
+            fontWeight: "400",
+            "border-radius": "0.25rem"
+          },
+          "code::before": {
+            content: '""',
+            "padding-left": "0.25rem"
+          },
+          "code::after": {
+            content: '""',
+            "padding-right": "0.25rem"
+          },
+          },
+        },
+      }),
+    },
   },
+  
   variants: {
     extend: {},
   },
